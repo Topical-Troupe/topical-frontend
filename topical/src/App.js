8a6ee00dfd2d845af3f39e0541b649751e6c3965
Alex Corey <alex.swansboro@gmail.com>
2020-07-29 14:35:13 -0400

    Routes to product list properly

--- topical/src/App.js
+++ topical/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import BarcodeSearch from './components/BarcodeSearch'
@@ -14,17 +14,30 @@ import Theme from './components/Theme'
 import ProductList from './components/ProductListPage'
 
 function App ({ result }) {
+
+
+  const [searchResults, setSearchResults] = useState(null)
+  console.log(searchResults, 'search Results')
+
+
+
   return (
     <div>
       <ThemeProvider theme={Theme}>
         <Router>
           <Navigation />
           <Switch>
-            <Route path='/' exact component={Home} />
+            <Route path = '/' exact>
+              <Home setSearchResults={setSearchResults}/>
+            </Route>
+{/* I recommend following the component pattern on line 28 so that it is a bit easier to pass props for the components below             */}
             <Route path='/profile' component={ProfilePage} />
             <Route path='/search' component={BarcodeSearch} />
             <Route path='/about' component={About} />
-            <Route path='/productlist/' render={() => <ProductList result={result} />} />
+            <Route path='/productlist/'>
+              <ProductList result={searchResults} />
+            </Route>
+
           </Switch>
         </Router>
       </ThemeProvider>
